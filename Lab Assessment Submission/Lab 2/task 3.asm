85ddd959a78f0ae770f9056f2b96c4eb1fc0ae06
Nafisa Tafshir <nafisa.tafshir@northsouth.edu>
2020-11-18 09:53:31 +0600

    task 3 4 submitted

---
+++ Lab Assessment Submission/Lab 2/task 3.asm
@@ -0,0 +1,18 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+
+org 100h
+
+array DB 5 DUP(8)
+
+ret
+
+
+
+
+;3. Define an array of 5 numbers using DUP
+
+;4. Define an array with occuring 1 and 2 value
+;(e.g- 1,2,1,2...)
---
+++ Lab Assessment Submission/Lab 2/task 4.asm
@@ -0,0 +1,21 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+
+
+
+org 100h
+
+array DB 3 DUP(1,2)
+
+ret
+
+
+
+
+
+;3. Define an array of 5 numbers using DUP
+
+;4. Define an array with occuring 1 and 2 value
+;(e.g- 1,2,1,2...)
